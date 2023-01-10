4eeb10011d716dd2ac54b285a87379dac6457338
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-10 16:26:59 +0100

    exercise done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default{
+  components: {
+    AppTitle
+  },
+  data(){
+    return{
+
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <AppTitle />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+       name: 'AppTitle',
+       data(){
+        return{
+            sentence:  "La mia prima app con Vite!"
+        }
+       }     
+    }
+</script>
+
+<template lang="">
+    <div>
+       {{ sentence }}
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
